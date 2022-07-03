a92465127e4746bf43b1509c3de8006964e8ab93
Aakriti <aakriti.poudel@accolitedigital.com>
2022-07-03 08:45:24 +0545

    DRY,refactoring and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
 	def show
-		@article=Article.find(params[:id])
 	end
 	def index
 		@articles=Article.all
@@ -10,12 +10,12 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article=Article.find(params[:id])
+		# @article=Article.find(params[:id])
 	end
 
 
 	def create
-		@article=Article.new(params.require(:article).permit(:title, :description))
+		@article=Article.new(create_update_article_params)
 		if @article.save
 			flash[:notice]="Article was created successfully!"
 			redirect_to @article
@@ -25,8 +25,8 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article=Article.find(params[:id])
-		if @article.update(params.require(:article).permit(:title, :description))
+		# @article=Article.find(params[:id])
+		if @article.update(create_update_article_params)
 			flash[:notice]="Article was updated successfully!"
 			redirect_to @article
 		else
@@ -36,9 +36,18 @@ class ArticlesController < ApplicationController
 
 
 	def destroy
-		@article=Article.find(params[:id])
+		# @article=Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
+	end
+
+	private
+	def set_article
+		@article=Article.find(params[:id])
+	end
+
+	def create_update_article_params
+		params.require(:article).permit(:title, :description)
 	end
 
 end
